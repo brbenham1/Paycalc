2fce8c901535d3a5fbcd938f5394b2d3cb48f357
brbenham1 <brbenham1@gmail.com>
2023-12-13 13:16:32 +1000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<h1 className="text-blue p-4 text-3xl font-bold underline">
+			Braith is Gammon
+		</h1>
+	);
+}
+
+export default App;
