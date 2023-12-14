297037925ee686ffa4bfabc820375504d88649eb
brbenham1 <brbenham1@gmail.com>
2023-12-15 09:51:57 +1000

    Modified day of the week in shift details

--- src/components/ShiftDetails.tsx
+++ src/components/ShiftDetails.tsx
@@ -4,10 +4,20 @@ export default function ShiftDetails() {
 	return (
 		<div className=" bg-base-300 shadow-base-content rounded-xl drop-shadow-md filter">
 			<form className="p-2">
-				<div className="mb-2">
-					<label className="mr-2">Day of the week: </label>
-					<input type="text" name="dayOfWeek" />
-				</div>
+				<label className="form-control w-full max-w-xs pb-2">
+					<div className="label">
+						<span className="label-text font-medium">Day of the Week</span>
+					</div>
+					<select className="select bg-primary-300 rounded-lg font-mono">
+						<option selected>Monday</option>
+						<option>Tuesday</option>
+						<option>Wednesday</option>
+						<option>Thursday</option>
+						<option>Friday</option>
+						<option>Saturday</option>
+						<option>Sunday</option>
+					</select>
+				</label>
 				<div className="mb-2">
 					<label className="mr-2">Starting Time:</label>
 					<input type="text" name="startTime" />
